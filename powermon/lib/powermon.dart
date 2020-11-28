38102fe7d600436cb9ad890df0ff1a8c89ee7d02
Bhamidipati <suryanarayana.bhamidipati@siemens.com>
2020-11-28 15:34:48 +0530

    complete the class

--- powermon/lib/powermon.dart
+++ powermon/lib/powermon.dart
@@ -3,10 +3,6 @@
 // To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
 // directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
 
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
 import 'package:powermon_platform_interface/powermon_platform_interface.dart';
 
 class Powermon {
