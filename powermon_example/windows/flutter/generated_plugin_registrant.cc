98a90785fc42195173581157e9b62aec6b8dd6f8
Bhamidipati <suryanarayana.bhamidipati@siemens.com>
2020-11-28 15:35:05 +0530

    adding example folder

---
+++ powermon_example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <powermon_example/powermon_example_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  PowermonExamplePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PowermonExamplePlugin"));
+}
